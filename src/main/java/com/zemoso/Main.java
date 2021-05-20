b45ef3d53c30b8820e5e4293e208cff7e32b491a
Abhijeet Mishra <abhijeet.mishra@zemosolabs.com>
2021-05-20 10:31:18 +0530

    Matching regex with only file names

--- src/main/java/com/zemoso/Main.java
+++ src/main/java/com/zemoso/Main.java
@@ -3,12 +3,8 @@ package com.zemoso;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,7 +13,7 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         File f = new File("/home");
-        String[] extensions = {"txt","log"};
+        String[] extensions = {"txt","log","java"};
         //reading only particular extensions
         Collection<File> files = FileUtils.listFiles(f, extensions, true);
         Iterator<File> iterator = files.iterator();
@@ -30,28 +26,12 @@ public class Main {
         while (iterator.hasNext()) {
             File tempFile = iterator.next();
             Pattern p = Pattern.compile(regex);
-            String allLines = getAllLines(tempFile);
-            if(allLines==null)continue;
-            Matcher m = p.matcher(allLines);
-            while(m.find()) {
-                String output = String.format("Found '%s' at %d, %d in file:%s",m.group(),m.start(),m.end(),tempFile.getName());
+            Matcher m = p.matcher(tempFile.getName());
+            if (m.find()) {
+                String output = String.format("Found '%s' at %d, %d in file:%s", m.group(), m.start(), m.end(), tempFile.getName());
                 System.out.println(output);
             }
         }
     }
 
-    /**
-     * Gets all lines from a file
-     * @param file File to get text from
-     * @return Contents of File as single String
-     */
-    private static String getAllLines(File file) {
-        try {
-            List<String> strings = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
-            return String.join(System.lineSeparator(), strings);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-        }
 }
