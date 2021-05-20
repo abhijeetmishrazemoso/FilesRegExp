9f1d807c51360315424d9d658d07a92fa386c16a
Abhijeet Mishra <abhijeet.mishra@zemosolabs.com>
2021-05-20 10:47:42 +0530

    working perfect

--- src/main/java/com/zemoso/Main.java
+++ src/main/java/com/zemoso/Main.java
@@ -13,24 +13,30 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         File f = new File("/home");
-        String[] extensions = {"txt","log","java"};
-        //reading only particular extensions
+        String[] extensions = {"txt", "log", "java"};//only these extensions files will be loaded
+        //FileUtils utility from Apache commons, with recursive -true flag, recursively gives all files.
         Collection<File> files = FileUtils.listFiles(f, extensions, true);
-        Iterator<File> iterator = files.iterator();
+        //reads the user input from stdin
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Regex:Type 'exit' to Exit:");
         String regex = sc.next();
-        if (regex.equals("exit")) {
-            System.exit(1);
-        }
-        while (iterator.hasNext()) {
-            File tempFile = iterator.next();
-            Pattern p = Pattern.compile(regex);
-            Matcher m = p.matcher(tempFile.getName());
-            if (m.find()) {
-                String output = String.format("Found '%s' at %d, %d in file:%s", m.group(), m.start(), m.end(), tempFile.getName());
-                System.out.println(output);
+        while (!regex.equals("exit")) {
+            if (regex.equals("exit")) {
+                break;
             }
+            Iterator<File> iterator = files.iterator();
+            while (iterator.hasNext()) {
+                File tempFile = iterator.next();
+                Pattern p = Pattern.compile(regex);
+                Matcher m = p.matcher(tempFile.getName());
+                if (m.find()) {
+                    String output = String.format("Found '%s' at %d, %d in file:%s",
+                            m.group(), m.start(), m.end(), tempFile.getName());
+                    System.out.println(output);
+                }
+            }
+            System.out.println("Enter Another regex:type 'exit' to exit:");
+            regex = sc.next();
         }
     }
 
